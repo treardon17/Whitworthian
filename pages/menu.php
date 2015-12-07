73e1e21ce279bc6090b3e84f5af91ab824548076
treardon17 <tdr.reardon@gmail.com>
2015-12-06 16:00:22 -0800

    Hovering over news boxes now has author profile pic

--- pages/menu.php
+++ pages/menu.php
@@ -1,3 +1,7 @@
 <nav id="nav-bar" class="menu-hiding">
+	<form id="search-box" class="nav-item nav-item-hiding">
+	    <input id="search-input" type="text" placeholder="Search..." required>
+	    <input type="button" value="Search">
+	</form>
 	<?php include("menu-items.php") ?>
 </nav>
--- pages/newspage.php
+++ pages/newspage.php
@@ -9,82 +9,95 @@
 <h1 class="title-heading">this week's edition</h1>
 
 <div class="news-box-container">
-	<div class="news-box grayscale high-priority-news" style="background-image: url('images/articleimages/doorknob.JPG')">
+	<div class="news-box">
+		<div class="news-box-background grayscale" style="background-image: url('images/articleimages/doorknob.JPG')"></div>
 		<a href="http://www.google.com" class="news-box-link"></a>
 		<div class="news-box-title-external-box">
 			<h3 class="news-box-title-external">This is a title</h3>
 		</div>
 		<div class="news-box-info">
 			<h3 class="news-box-title">This is a title</h3>
+			<img class="news-box-author-image" alt="Katie Shaw" src="images/authorimages/MediaSpecialist.png">
 			<h4 class="news-box-author">Katie Shaw</h4>
 			<p class="news-box-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 		</div>
 	</div>
-	<div class="news-box grayscale low-priority-news" style="background-image: url('images/articleimages/baptism.JPG')">
+	<div class="news-box">
+		<div class="news-box-background grayscale" style="background-image: url('images/articleimages/ball.JPG')"></div>
 		<a href="http://www.google.com" class="news-box-link"></a>
 		<div class="news-box-title-external-box">
 			<h3 class="news-box-title-external">This is a title</h3>
 		</div>
 		<div class="news-box-info">
 			<h3 class="news-box-title">This is a title</h3>
+			<img class="news-box-author-image" alt="Katie Shaw" src="images/authorimages/MediaSpecialist.png">
 			<h4 class="news-box-author">Katie Shaw</h4>
 			<p class="news-box-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 		</div>
 	</div>
-	<div class="news-box grayscale medium-priority-news" style="background-image: url('images/articleimages/puddle.JPG')">
+	<div class="news-box">
+		<div class="news-box-background grayscale" style="background-image: url('images/articleimages/band.JPG')"></div>
 		<a href="http://www.google.com" class="news-box-link"></a>
 		<div class="news-box-title-external-box">
 			<h3 class="news-box-title-external">This is a title</h3>
 		</div>
 		<div class="news-box-info">
 			<h3 class="news-box-title">This is a title</h3>
+			<img class="news-box-author-image" alt="Katie Shaw" src="images/authorimages/MediaSpecialist.png">
 			<h4 class="news-box-author">Katie Shaw</h4>
 			<p class="news-box-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 		</div>
 	</div>
-	<div class="news-box grayscale medium-priority-news" style="background-image: url('images/articleimages/ball.JPG')">
+	<div class="news-box">
+		<div class="news-box-background grayscale" style="background-image: url('images/articleimages/baptism.JPG')"></div>
 		<a href="http://www.google.com" class="news-box-link"></a>
 		<div class="news-box-title-external-box">
 			<h3 class="news-box-title-external">This is a title</h3>
 		</div>
 		<div class="news-box-info">
 			<h3 class="news-box-title">This is a title</h3>
+			<img class="news-box-author-image" alt="Katie Shaw" src="images/authorimages/MediaSpecialist.png">
 			<h4 class="news-box-author">Katie Shaw</h4>
 			<p class="news-box-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 		</div>
 	</div>
-	<div class="news-box grayscale low-priority-news" style="background-image: url('images/articleimages/pinecone.JPG')">
+	<div class="news-box">
+		<div class="news-box-background grayscale" style="background-image: url('images/articleimages/sunset.JPG')"></div>
 		<a href="http://www.google.com" class="news-box-link"></a>
 		<div class="news-box-title-external-box">
 			<h3 class="news-box-title-external">This is a title</h3>
 		</div>
 		<div class="news-box-info">
 			<h3 class="news-box-title">This is a title</h3>
+			<img class="news-box-author-image" alt="Katie Shaw" src="images/authorimages/MediaSpecialist.png">
 			<h4 class="news-box-author">Katie Shaw</h4>
 			<p class="news-box-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 		</div>
 	</div>
-	<div class="news-box grayscale low-priority-news" style="background-image: url('images/articleimages/band.JPG')">
+	<div class="news-box">
+		<div class="news-box-background grayscale" style="background-image: url('images/articleimages/giraffe.JPG')"></div>
 		<a href="http://www.google.com" class="news-box-link"></a>
 		<div class="news-box-title-external-box">
 			<h3 class="news-box-title-external">This is a title</h3>
 		</div>
 		<div class="news-box-info">
 			<h3 class="news-box-title">This is a title</h3>
+			<img class="news-box-author-image" alt="Katie Shaw" src="images/authorimages/MediaSpecialist.png">
 			<h4 class="news-box-author">Katie Shaw</h4>
 			<p class="news-box-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 		</div>
 	</div>
-	<div class="news-box grayscale low-priority-news" style="background-image: url('images/articleimages/giraffe.JPG')">
+	<div class="news-box">
+		<div class="news-box-background grayscale" style="background-image: url('images/articleimages/pinecone.JPG')"></div>
 		<a href="http://www.google.com" class="news-box-link"></a>
 		<div class="news-box-title-external-box">
 			<h3 class="news-box-title-external">This is a title</h3>
 		</div>
 		<div class="news-box-info">
 			<h3 class="news-box-title">This is a title</h3>
+			<img class="news-box-author-image" alt="Katie Shaw" src="images/authorimages/MediaSpecialist.png">
 			<h4 class="news-box-author">Katie Shaw</h4>
 			<p class="news-box-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 		</div>
 	</div>
-	
 </div>
